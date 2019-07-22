d56e8004ae076ab47dd19b65a03d74aef53d32c0
Aroldo <aroldo@digital.gob.cl>
2019-07-22 16:30:29 -0400

    fix on relativeTime filter

--- src/filters.js
+++ src/filters.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import moment from 'moment'
 
 Vue.filter('relativeTime', (value) => {
-    return moment(value).startOf('hour').fromNow();
+    let timestamp = moment(value).valueOf();
+    return moment(timestamp).startOf('minutes').fromNow();
 });
 
 Vue.filter('toCLP', function (price) {
 	price = (price/1).toFixed(0).replace('.', ',')
     price = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-
     return `$${price}`;
 });
